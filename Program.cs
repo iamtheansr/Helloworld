54c6a24e26bf6e3a9c868d692b7cdf4f468bc124
iamtheansr <iamtheansr@gmail.com>
2024-08-03 01:22:15 +0200

    i have a method

--- Program.cs
+++ Program.cs
@@ -2,9 +2,15 @@
 {
     internal class Program
     {
+        public class Person
+        {
+            public void Method()
+            {
+                Console.WriteLine("Somebody made a method");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello johnny two shoes");
         }
     }
 }
