ae7f890e3fcdfe83319889ccb37c69aaa2c521f9
iamtheansr <iamtheansr@gmail.com>
2024-08-03 01:02:12 +0200

    one message

--- Program.cs
+++ Program.cs
@@ -5,21 +5,6 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
-            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
-            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
-            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
-            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
-            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
-            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
-            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
-            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
-            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
-            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
-            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
-            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
-            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
-            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
-
         }
     }
 }
