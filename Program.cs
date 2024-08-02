db574fd6ef896c0fcbcd40c9424846893953b906
iamtheansr <iamtheansr@gmail.com>
2024-08-03 00:56:11 +0200

    Fourth mod

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello johnny two shoes");
+            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
         }
     }
 }
