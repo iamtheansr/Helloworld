5e48a2663c5b78e1ab4246d90f7eda3ba3d05b62
iamtheansr <iamtheansr@gmail.com>
2024-08-03 01:15:12 +0200

    Revert "Fourth mod"
    This reverts commit db574fd6ef896c0fcbcd40c9424846893953b906.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Heloo, my good friend, I see you are on the hub at 00:55 on a Saturday morning");
+            Console.WriteLine("Hello johnny two shoes");
         }
     }
 }
