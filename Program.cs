559b0cad9ef843de0e86d98b25568feca19c98a9
iamtheansr <iamtheansr@gmail.com>
2024-08-03 00:51:59 +0200

    Second mod

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello universe bitches");
+            Console.WriteLine("Hello what's good ma dude");
         }
     }
 }
